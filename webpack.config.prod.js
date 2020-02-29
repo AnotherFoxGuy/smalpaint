685c10bbbbc56f4de26cbcec43ce1c297a6df046
Restyled.io <commits@restyled.io>
2020-02-29 10:35:33 +0000

    Restyled by prettier

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,26 +1,24 @@
-const merge = require('webpack-merge');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const common = require('./webpack.common.js');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const merge = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const common = require("./webpack.common.js");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
-  mode : 'production',
-  devtool : false,
-  bail : true,
-  output : {
-    filename : 'js/[name].[chunkhash:8].js',
-    chunkFilename : 'js/[name].[chunkhash:8].chunk.js'
+  mode: "production",
+  devtool: false,
+  bail: true,
+  output: {
+    filename: "js/[name].[chunkhash:8].js",
+    chunkFilename: "js/[name].[chunkhash:8].chunk.js"
   },
-  module : {
-    rules : [
+  module: {
+    rules: [
       {
-        test : /\.svelte$/,
-        use : {loader : 'svelte-loader', options : {emitCss : true}}
+        test: /\.svelte$/,
+        use: { loader: "svelte-loader", options: { emitCss: true } }
       },
-      {test : /\.css$/, use : [ MiniCssExtractPlugin.loader, 'css-loader' ]}
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] }
     ]
   },
-  plugins : [
-    new CleanWebpackPlugin(),
-  ]
+  plugins: [new CleanWebpackPlugin()]
 });
