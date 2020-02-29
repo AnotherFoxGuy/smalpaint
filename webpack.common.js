2860860e46b29dba9a464351d1f2977a856f2ea3
Restyled.io <commits@restyled.io>
2020-02-29 10:35:31 +0000

    Restyled by clang-format

--- webpack.common.js
+++ webpack.common.js
@@ -6,39 +6,23 @@ const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: {
-        bundle: ['./src/main.js']
-    },
-    resolve: {
-        alias: {
-            svelte: path.resolve('node_modules', 'svelte')
-        },
-        extensions: ['.mjs', '.js', '.svelte'],
-        mainFields: ['svelte', 'browser', 'module', 'main']
-    },
-    output: {
-        path: __dirname + '/build'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader"
-            }
-        ]
-    },
-    plugins: [
-        new CopyPlugin([
-            { from: 'public', to: '.' },
-        ]),
-        new webpack.ProgressPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html',
-            inject: true
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[chunkhash:8].css'
-        })
-    ],
+  entry : {bundle : [ './src/main.js' ]},
+  resolve : {
+    alias : {svelte : path.resolve('node_modules', 'svelte')},
+    extensions : [ '.mjs', '.js', '.svelte' ],
+    mainFields : [ 'svelte', 'browser', 'module', 'main' ]
+  },
+  output : {path : __dirname + '/build'},
+  module : {rules : [ {test : /\.tsx?$/, loader : "ts-loader"} ]},
+  plugins : [
+    new CopyPlugin([
+      {from : 'public', to : '.'},
+    ]),
+    new webpack.ProgressPlugin(), new HtmlWebpackPlugin({
+      filename : 'index.html',
+      template : './src/index.html',
+      inject : true
+    }),
+    new MiniCssExtractPlugin({filename : 'css/[name].[chunkhash:8].css'})
+  ],
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,34 +4,23 @@ const common = require('./webpack.common.js');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
-    mode: 'production',
-    devtool: false,
-    bail: true,
-    output: {
-        filename: 'js/[name].[chunkhash:8].js',
-        chunkFilename: 'js/[name].[chunkhash:8].chunk.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.svelte$/,
-                use: {
-                    loader: 'svelte-loader',
-                    options: {
-                        emitCss: true
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
+  mode : 'production',
+  devtool : false,
+  bail : true,
+  output : {
+    filename : 'js/[name].[chunkhash:8].js',
+    chunkFilename : 'js/[name].[chunkhash:8].chunk.js'
+  },
+  module : {
+    rules : [
+      {
+        test : /\.svelte$/,
+        use : {loader : 'svelte-loader', options : {emitCss : true}}
+      },
+      {test : /\.css$/, use : [ MiniCssExtractPlugin.loader, 'css-loader' ]}
     ]
+  },
+  plugins : [
+    new CleanWebpackPlugin(),
+  ]
 });
